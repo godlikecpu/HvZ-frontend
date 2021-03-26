3b684696d17b8568b85c99ca7534f0d136c612d2
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2021-03-26 09:43:13 +0100

    feat: add create game function

---
+++ src/components/create-game/CreateGame.jsx
@@ -0,0 +1,42 @@
+import InputField from "./InputField";
+import { useState } from "react";
+import { postGame } from "../../utils/apiFetcher";
+
+const CreateGame = () => {
+
+    const getState = (state, id) => {
+        setRequestBody({...requestBody, [id]: state});
+    };
+
+    const [requestBody, setRequestBody] = useState(
+        {
+            name: "", 
+            northWestLatitude: "",
+            northWestLongitude: "",
+            southEastLatitude: "",
+            southEastLongitude: "",
+            gameState: "REGISTRATION"
+        }    
+    )
+
+    const createGame = () => {
+        postGame(requestBody.name, requestBody.northWestLatitude, requestBody.northWestLongitude,
+            requestBody.southEastLatitude, requestBody.southEastLongitude, requestBody.gameState
+        );
+    };
+
+    return (
+      <>
+        <div>
+        <InputField id="name" getState={getState} placeholder="Write name of the game" />
+        <InputField id="northWestLatitude" getState={getState} placeholder="Give North-West Latitude" />
+        <InputField id="northWestLongitude" getState={getState} placeholder="Give North-West Longitude" />
+        <InputField id="southEastLatitude" getState={getState} placeholder="Give South-East Latitude" />
+        <InputField id="southEastLongitude" getState={getState} placeholder="Give South-East Longitude" />
+        </div>
+        <button type="button" onClick={createGame}>Create a game</button>
+      </>
+    );
+  };
+ 
+export default CreateGame;
