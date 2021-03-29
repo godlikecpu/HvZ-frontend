c52511a6f2e9a50eaebd3ae30fa5ffd9660c4fe1
GodlikeCPU <dan.tomicic@gmail.com>
2021-03-29 10:14:40 +0200

    fix: GamePage is now not dependant on location props

--- src/components/pages/GamePage.jsx
+++ src/components/pages/GamePage.jsx
@@ -5,35 +5,17 @@ import { useState, useEffect } from "react";
 const GamePage = (props) => {
   const [game, setGame] = useState({});
   const [kills, setKills] = useState([]);
-  const gameId = props.location.pathname.split("/")[2];
+  const gameId = window.location.href.split("/")[4];
+
   useEffect(() => {
-    if (props.location.gameProps) {
-      const gameData = props.location.gameProps;
+    fetchGame(gameId).then((gameData) => {
       setGame(gameData);
       gameData.kills.map(async (apiCall) => {
         await fetch(apiCall).then((kill) => {
           setKills((kills) => [...kills, kill]);
         });
       });
-    } else {
-      fetchGame(gameId).then((gameData) => {
-        setGame(gameData);
-        gameData.kills.map(async (apiCall) => {
-          await fetch(apiCall).then((kill) => {
-            setKills((kills) => [...kills, kill]);
-          });
-        });
-      });
-    }
-
-    // (async () => {
-    //   setGame(await fetchGame(gameId));
-    //   setKills(
-    //     game.kills.map(async (apiCall) => {
-    //       return fetch(apiCall);
-    //     })
-    //   );
-    // })();
+    });
 
     // eslint-disable-next-line
   }, []);
